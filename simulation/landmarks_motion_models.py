cb845a903f48be26003ff2144a4eaa6f6aeeafa8
surenkum <surenkum@buffalo.edu>
2015-04-30 17:32:11 -0400

    Completed code for prismatic motion model

--- simulation/landmarks_motion_models.py
+++ simulation/landmarks_motion_models.py
@@ -2,6 +2,7 @@
 Given trajectories, estimate the motion models currently being followed by the landmarks
 """
 import numpy as np
+from scipy.optimize import minimize
 import pdb
 
 # Abstract class for storing and estimating various motion models
@@ -21,10 +22,10 @@ class Motion_Models:
         self.num_data = self.num_data+1
         if self.num_data<self.min_data_samples:
             # Add data to model data
-            self.model_data[self.num_data-1,:] = inp_data
+            self.model_data[self.num_data-1,:] = inp_data.copy()
         elif self.num_data==self.min_data_samples:
             # Add data and call fit_model function
-            self.model_data[self.num_data-1,:] = inp_data
+            self.model_data[self.num_data-1,:] = inp_data.copy()
             self.fit_model()
         else:
             # Update the stored data as well
@@ -76,16 +77,28 @@ class Move_Revolute(Motion_Models):
 
 
 # Landmark motion model that is moving along a line with specified velocity
-class Move_Prismatic(Motion_Models):
+class Prismatic_Landmark(Motion_Models):
     # Model paramters for a static landmark is just the location of the landmark
     def fit_model(self):
         # asserting that we have atleast the minimum required observation for estimation
         assert(self.num_data>=self.min_data_samples),"Can not call this function until we have sufficient data to estimate the model"
         # Fitting a prismatic model using maximum likelihood
-        # Model consists of x_0,y_0,m,c,v_l : Starting x, starting y, slope, intercept, velocity along line
+        # Model consists of x_0,y_0,\theta,v_l : Starting x, starting y, slope, velocity along line
+        # Model is x_k = n*v_l\cos(\theta)+x_0, y_k = n*v_l\cos(\theta)+y_0 where n = 0,...,t are time steps
         print "Fitting the motion model"
-        self.model_par = self.model_data[-1,:].copy()
+        v_l_guess = np.sqrt((self.model_data[1,0]-self.model_data[0,0])**2+(self.model_data[1,1]-self.model_data[0,1])**2)
+        x0 = np.array([self.model_data[0,0],self.model_data[0,1],0,v_l_guess])
+        res = minimize(self.ml_model,x0,options={'xtol':1e-8,'disp': True})
+        self.model_par = res.x
         print "Estimated Model paramters are", self.model_par
+
+    # Defining the maximum likelihood model
+    def ml_model(self,x):
+        sum_func = 0
+        for i in range(self.min_data_samples):
+            sum_func = sum_func+(self.model_data[i,0]-((i*x[3]*np.cos(x[2]))+x[0]))**2+(self.model_data[i,1]-((i*x[3]*np.sin(x[2]))+x[1]))**2
+        return sum_func
+        
     def update_model_pars(self):
         # Asserting that we have a model
         assert(self.model_par is not None),'Do not call this functin until we have sufficient data to estimate a model'
@@ -96,17 +109,22 @@ class Move_Prismatic(Motion_Models):
         # Asserting that we have a model
         assert(self.model_par is not None),'Do not call this functin until we have sufficient data to estimate a model'
         # Predicting the location of the static landmark by adding gaussian noise
-        return np.random.multivariate_normal(self.model_par,noise_cov)
+        mean_x = self.num_data*self.model_par[3]*np.cos(self.model_par[2])+self.model_par[0]
+        mean_y = self.num_data*self.model_par[3]*np.sin(self.model_par[2])+self.model_par[1]
+        return np.random.multivariate_normal(np.array([mean_x,mean_y]),noise_cov)
 
 
 if __name__=="__main__":
     # Lets simulate some data from static sensors
-    data = np.array([1,2])
+    data = np.array([0,0])
     noise_cov = np.diag([0.05,0.05])
-    model1 = Static_Landmark(1,noise_cov)
+    model1 = Prismatic_Landmark(2,noise_cov)
     model1.process_inp_data(data)
-    print model1.model_par,model1.predict_model(),model1.model_data
-    data1 = np.array([1.2,3.2])
+    print model1.model_par,model1.model_data
+    data1 = np.array([1,0])
     model1.process_inp_data(data1)
-    print model1.model_par,model1.predict_model(),model1.model_data
+    print model1.model_par,model1.model_data,model1.predict_model()
+    data2 = np.array([2,0])
+    model1.process_inp_data(data2)
+    print model1.model_par,model1.model_data
 
