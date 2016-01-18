189e4b13cd99ce83d2da52a2332d880dd55aaccf
Madan Ravi Ganesh <madantrg@umich.edu>
2016-01-18 18:22:40 -0500

    Added robot conic view function, tested seen and out of view static land marks

--- simulation/test_3d_models.py
+++ simulation/test_3d_models.py
@@ -13,7 +13,7 @@ import utils_plot as up
 import scipy.linalg
 import matplotlib.pyplot as plt
 
-def gen_data(model='rev'):
+def gen_simple_data(model='rev'):
     # Generate output with initial point appended
     if model=='rev':
         # Model parameters
@@ -35,17 +35,41 @@ def gen_data(model='rev'):
     elif model=='static':
         x_0 = 2.0
         y_0 = 2.0
+        z_0 = 0.0
         for i in range(30):
-            yield np.array([x_0,y_0,1,x_0,y_0,1])
+            yield np.array([x_0,y_0,z_0,x_0,y_0,z_0])
 
     else:
         x_0 = 2.0
         y_0 = 2.0
-        v_y = 0.2
-        v_x = 0.1
+        v_y = 0.0
+        v_x = 5.0
         for i in range(30):
-            yield np.array([x_0+i*v_x,y_0+i*v_y,1,x_0,y_0,1])
+            yield np.array([x_0+i*v_x,y_0+i*v_y,0,x_0,y_0,0])
 
+def in_view(rob_state,ldmk_pos):
+    #pt = np.array([2.0,2.0,0.0])
+    pt = ldmk_pos
+    #apex = np.array([1.0,5.0,0.0])
+    apex = np.array([rob_state[0],rob_state[1],0.0])
+    max_dist = 20
+    phi_view = 90*np.pi/180 # Assumption of robot looking in plane (flat) 
+    theta_view = rob_state[2] # robot_state[2] 
+    aperture = 45*np.pi/180
+    basement = np.array([max_dist*np.cos(theta_view)*np.sin(phi_view),
+                  max_dist*np.sin(theta_view)*np.sin(phi_view),
+                 max_dist*np.cos(phi_view)])
+                  
+    basement = basement + np.array(apex)
+  
+      
+    ap2vec = apex - pt
+    axisvec = apex - basement
+     
+    X = np.around(ap2vec.dot(axisvec)/np.linalg.norm(axisvec)/np.linalg.norm(ap2vec),3) >= np.around(np.cos(aperture),3)
+  
+    Y = np.around(ap2vec.dot(axisvec)/np.linalg.norm(axisvec),3) <= np.around(np.linalg.norm(axisvec))
+    return X and Y                        
 
 def test():
     # Generate the test data
@@ -81,58 +105,43 @@ def test():
     #        lk_pred = ldmk_am[0].predict_model(motion_class.means[0])		
     #   	    print lk_pred,obs[0:3]
     
-    data = gen_data('pris')
+    data = gen_simple_data('prismatic')
     # Case 1: robot at origin
-    #robot_state = np.array([0,0,0])
+    robot_state = np.array([0,0,np.pi*90/180])
 
     # Case 2: Robot away from origin with some theta
-    robot_state = np.array([1,5,np.pi])
+    #robot_state = np.array([1,5,np.pi*90/180])
     for packet in data:
         
         curr_obs = packet[0:3]
         init_pt = packet[3:]
 
-        motion_class = ldmk_estimater.setdefault(0, mm.Estimate_Mm())
+        m_thresh = 0.6
+        if in_view(robot_state,curr_obs):
+            motion_class = ldmk_estimater.setdefault(0, mm.Estimate_Mm())
 
-        chosen_am = ldmk_am.setdefault(0,None)
+            chosen_am = ldmk_am.setdefault(0,None)
 
-        motion_class.process_inp_data([0,0],robot_state,curr_obs,init_pt)
-        if ldmk_am[0] is None:
-        # Still estimating model
-            if sum(motion_class.prior>0.6)>0:
-                model = np.where(motion_class.prior>0.6)[0]
-                ldmk_am[0] = motion_class.am[model]
+            motion_class.process_inp_data([0,0],robot_state,curr_obs,init_pt)
+            if ldmk_am[0] is None:
+            # Still estimating model
+                if sum(motion_class.prior>m_thresh)>0:
+                    model = np.where(motion_class.prior>m_thresh)[0]
+                    ldmk_am[0] = motion_class.am[model]
+            else:
+                # LK_pred comes out in the robot_frame. We need to convert it back to world frame to match the world coordinate observations
+                lk_pred = ldmk_am[0].predict_model(motion_class.means[model])
+                
+                R_temp = np.array([[np.cos(-robot_state[2]), -np.sin(-robot_state[2]),0],
+                     [np.sin(-robot_state[2]), np.cos(-robot_state[2]),0],
+                     [0,0,1]])
+                
+                pos_list = np.ndarray.tolist(robot_state[0:2]) 
+                pos_list.append(0.0)
+                print motion_class.prior
+                print model,R_temp.T.dot(lk_pred)+np.array(pos_list),curr_obs        
         else:
-            #pdb.set_trace() 
-            # LK_pred comes out in the robot_frame. We need to convert it back to world frame to match the world coordinate observations
-            lk_pred = ldmk_am[0].predict_model(motion_class.means[model])
-            
-            R_temp = np.array([[np.cos(robot_state[2]), -np.sin(robot_state[2]),0],
-                 [np.sin(robot_state[2]), np.cos(robot_state[2]),0],
-                 [0,0,1]])
-            
-            pos_list = np.ndarray.tolist(robot_state[0:2]) 
-            pos_list.append(0.0)
-                   
-            print model,R_temp.T.dot(lk_pred)+np.array(pos_list),curr_obs        
-
-        """ Legacy code (examples)
-        # Revolute
-        #init_pt,curr_obs = np.array([r*np.cos(-i*w)+x_0,r*np.sin(-i*w)+y_0,1])
- 
-        # Static
-        #curr_obs = np.array([x_0,y_0,0])
- 
-         # Prismatic - starting from x_0,y_0 ,slope of line at w
-         #curr_obs = np.array([x_0-i*r*np.cos(w),y_0+i*r*np.sin(w)])
- 
-        
-        #print "Rev: ",motion_class.prior[0],"Pris: ",motion_class.prior[1],"Static: ",motion_class    .prior[2]
-        
-        #if i>8:
-        #    import pdb; pdb.set_trace()
-        #    print "Revolute joint observed position ",\
-        #    motion_class.am[0].predict_model(motion_class.means[0]), "obs = ",curr_obs"""
+            print "Not in view"
 
 
 
